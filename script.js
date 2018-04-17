5dfec6c45720f5aab19cbea955688753da7e1f37
Nick Nunez <nickn10@comcast.net>
2018-04-16 20:28:44 -0500

    Add second solution

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 const fs = require('fs');
 
+// Solution One 
+// fs.readFile('./input.txt',(err, data) => {
+//    console.time('timer');
+//    if(err) return (err);
+//    let floor = 0;
+//    const str = data.toString();
+//    let i = str.length - 1;
+//    while (i >=0) {
+//       str[i] === '(' ? floor++ : floor--;
+//       i--;
+//    }
+//    console.log(floor);
+//    console.timeEnd('timer'); // 2.7ms
+// });
+
+// Solution Two
 fs.readFile('./input.txt',(err, data) => {
    console.time('timer');
    if(err) return (err);
-   let floor = 0;
    const str = data.toString();
-   let i = str.length - 1;
-   while (i >=0) {
-      str[i] === '(' ? floor++ : floor--;
-      i--;
-   }
-   console.log(floor);
-   console.timeEnd('timer'); // 2.7
+   const up = str.match(/\(/g).length;
+   console.log(up - (str.length - up));
+   console.timeEnd('timer'); // 2.6ms
 });
